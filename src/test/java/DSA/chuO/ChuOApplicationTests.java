44e6973a72d1e65f2bd1831900f8b5186f3e4af5
krgrocon <krgrocon@gmail.com>
2024-06-22 16:05:20 +0900

    first commit

---
+++ src/test/java/DSA/chuO/ChuOApplicationTests.java
@@ -0,0 +1,13 @@
+package DSA.chuO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuOApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
